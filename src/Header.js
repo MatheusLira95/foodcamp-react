983006b039ceebecbc77f55b1bc90be4b499dce7
Matheus Lira <matheustlira@gmail.com>
2021-05-04 19:20:37 -0300

    Component Overlay/Header

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <>
+            <div class="header">
+                <div class="titulo">FoodCamp</div>
+                <div class="subtitulo">Sua comida em 6 minutos</div>
+            </div>
+        </>
+    );
+}
